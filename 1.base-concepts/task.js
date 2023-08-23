8b6e8eb65c090f2c03b8b008153c0bee23664015
Test <test@test.com>
2023-08-23 17:44:25 +0300

    First

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -24,38 +24,3 @@ const solutions3 = solveEquation(2, 1, 3);
 console.log(solutions1);
 console.log(solutions2);
 console.log(solutions3);
-
-
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  if (
-    typeof percent !== 'number' ||
-    typeof contribution !== 'number' ||
-    typeof amount !== 'number' ||
-    typeof countMonths !== 'number'
-  ) {
-    return false;
-  }
-
-  const monthlyInterestRate = percent / 100 / 12;
-
-  const loanAmount = amount - contribution;
-
-  const monthlyPayment =
-    loanAmount *
-    (monthlyInterestRate +
-      monthlyInterestRate / (Math.pow(1 + monthlyInterestRate, countMonths) - 1));
-
-  const totalPayment = monthlyPayment * countMonths;
-
-  const roundedTotalPayment = parseFloat(totalPayment.toFixed(2));
-
-  return roundedTotalPayment;
-}
-
-console.log(calculateTotalMortgage(10, 0, 50000, 12));
-console.log(calculateTotalMortgage(10, 1000, 50000, 12));
-console.log(calculateTotalMortgage(10, 0, 20000, 24));
-console.log(calculateTotalMortgage(10, 1000, 20000, 24));
-console.log(calculateTotalMortgage(10, 20000, 20000, 24));
-console.log(calculateTotalMortgage(10, 0, 10000, 36));
-console.log(calculateTotalMortgage(15, 0, 10000, 36));
